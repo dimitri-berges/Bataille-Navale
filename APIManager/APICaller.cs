f3e81e12a0157132d4c6ce671cf865d46d2e4a88
Dimitri B <dimitri.berges@lomis.fr>
2023-03-01 19:35:51 +0100

    Implémentation de l'APICaller afin de faire les appels API

---
+++ APIManager/APICaller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json;
+
+namespace APIManager
+{
+    public class APICaller
+    {
+        private static string API_URL = "https://api-lprgi.natono.biz/api/GetConfig";
+        public static string GetJSON()
+        {
+            var webClient = new System.Net.WebClient();
+            webClient.Headers.Add("x-functions-key", "lprgi_api_key_2023");
+            string jsonString = webClient.DownloadString(API_URL);
+            return jsonString;
+        }
+    }
+}
