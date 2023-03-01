abb645e9f33d2f3096b01f14c530634ace9c0078
Dimitri B <dimitri.berges@lomis.fr>
2023-03-01 19:50:59 +0100

    Ajout des tests unitaires pour le GameCore

--- GameCore/GameEngine.cs
+++ GameCore/GameEngine.cs
@@ -1,9 +1,16 @@
-﻿namespace GameCore
+﻿using System;
+
+namespace GameCore
 {
     public class GameEngine
     {
         public int nbLignes { get; set; }
         public int nbColonnes { get; set; }
         public Bateau[] bateaux { get; set; }
+
+        public static GameEngine FromJSON(string json)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Tests_GameCore/Tests_GameEngine.cs
@@ -0,0 +1,20 @@
+using GameCore;
+using System;
+using Xunit;
+
+namespace Tests_GameCore
+{
+    public class Tests_GameEngine
+    {
+        [Fact]
+        public void Test_GameEngineConstructorFromJSON()
+        {
+            GameEngine gameEngine = GameEngine.FromJSON("{\r\n  \"nbLignes\": 10,\r\n  \"nbColonnes\": 10,\r\n  \"bateaux\": [\r\n    {\r\n      \"taille\": 5,\r\n      \"nom\": \"Porte-avions\"\r\n    },\r\n    {\r\n      \"taille\": 4,\r\n      \"nom\": \"Croiseur\"\r\n    },\r\n    {\r\n      \"taille\": 3,\r\n      \"nom\": \"Contre-torpilleur\"\r\n    },\r\n    {\r\n      \"taille\": 3,\r\n      \"nom\": \"Sous-marin\"\r\n    },\r\n    {\r\n      \"taille\": 2,\r\n      \"nom\": \"Torpilleur\"\r\n    }\r\n  ]\r\n}");
+            Assert.NotNull(gameEngine);
+            Assert.IsType<GameEngine>(gameEngine);
+            Assert.Equal(10, gameEngine.nbLignes);
+            Assert.Equal(10, gameEngine.nbColonnes);
+            Assert.Equal(5, gameEngine.bateaux.Length);
+        }
+    }
+}
