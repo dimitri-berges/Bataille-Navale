55ef362f75436c02306fc951d58438d91a33410b
Dimitri B <dimitri.berges@lomis.fr>
2023-03-01 18:36:38 +0100

    Ajoutez des fichiers projet.

---
+++ InterfaceConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
