b888f992184914d9c2df3a01c1f86e501765ef0b
Emedo80 <emeric.dorion@gmail.com>
2023-03-22 19:29:57 +0100

    Ajout de classes : BateauEntity Pour complété les paramètres de la classe bateau Case et Case Status

---
+++ GameCore/BateauEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCore
+{
+    internal class BateauEntity : Bateau
+    {
+        public int x { get; internal set;}
+        public int y { get; internal set;}
+
+        internal List<Case> cases;
+
+        internal bool orientation;
+
+    }
+}
---
+++ GameCore/Case.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCore
+{
+    internal class Case
+    {
+        public CaseStatut Statut{ get; internal set; }
+        public int x, y;
+        internal Bateau? Bateau { get; set; }
+
+    }
+}
