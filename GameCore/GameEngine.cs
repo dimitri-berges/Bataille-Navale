b05ce840f017f3c6a019b9eb520c40aac4c5f549
Dimitri B <dimitri.berges@lomis.fr>
2023-05-03 18:42:44 +0200

    Renommage de Player en Joueur

--- GameCore/GameEngine.cs
+++ GameCore/GameEngine.cs
@@ -6,24 +6,36 @@ namespace GameCore
 {
     public class GameEngine
     {
+        #region Variables d'initialisation
         public int nbLignes { get; set; }
         public int nbColonnes { get; set; }
         public Bateau[] bateaux { get; set; }
-        public Player Player1 { get; set; }
-        public Player Player2 { get; set; }
-        public Player[] Players { get; set; }
+        public Joueur Joueur1 { get; set; }
+        public Joueur Joueur2 { get; set; }
+        #endregion
 
+        #region Variables de jeu
+        public Joueur[] Joueurs { get; set; }
+        public int NumeroJoueurActuel { get; private set; }
+        public Joueur JoueurActuel => Joueurs[NumeroJoueurActuel];
+        #endregion
+
+        #region Initialisation
         public static GameEngine FromJSON(string json)
         {
             return JsonSerializer.Deserialize<GameEngine>(json).Initialize();
         }
-
         private GameEngine Initialize()
         {
-            Player1 = new(new(nbLignes, nbColonnes), bateaux.ToArray());
-            Player2 = new(new(nbLignes, nbColonnes), bateaux.ToArray());
-            Players = new Player[] { Player1, Player2 };
+            Joueur1 = new(new(nbLignes, nbColonnes), bateaux.ToArray());
+            Joueur2 = new(new(nbLignes, nbColonnes), bateaux.ToArray());
+            Joueurs = new Joueur[] { Joueur1, Joueur2 };
             return this;
         }
+        #endregion
+
+        #region Méthodes
+
+        #endregion
     }
 }
--- GameCore/Joueur.cs
+++ GameCore/Joueur.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace GameCore
 {
-    public class Player
+    public class Joueur
     {
         public Plateau Plateau { get; private set; }
         public Plateau PlateauAdverse { get; private set; }
         public Bateau[] BateauxDisponibles { get; private set; }
         public BateauEntity[] Bateaux { get; private set; }
 
-        public Player(Plateau plateau, Bateau[] bateauxDisponibles) {
+        public Joueur(Plateau plateau, Bateau[] bateauxDisponibles) {
             Plateau = plateau;
             PlateauAdverse = new(plateau);
             BateauxDisponibles = bateauxDisponibles;
