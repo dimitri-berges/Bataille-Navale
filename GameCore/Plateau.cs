11ae45823dc66f34426579c6b8531ad91070dcea
Dimitri B <dimitri.berges@lomis.fr>
2023-05-15 19:43:36 +0200

    Correction d'un mélange entre les colonnes et les lignes

--- GameCore/Plateau.cs
+++ GameCore/Plateau.cs
@@ -12,13 +12,13 @@ namespace GameCore
         public List<Bateau> Bateaux { get; private set; }
 
         public Plateau(int nbLignes, int nbColonnes) {
-            Cases = new Case[nbLignes, nbColonnes];
+            Cases = new Case[nbColonnes, nbLignes];
             for (int i = 0; i < nbLignes; i++)
                 for (int j = 0; j < nbColonnes; j++)
-                    Cases[i, j] = new(i, j);
+                    Cases[j, i] = new(j, i);
         }
 
-        public Plateau(Plateau plateau) : this(plateau.Cases.GetLength(0), plateau.Cases.GetLength(1)) {}
+        public Plateau(Plateau plateau) : this(plateau.Cases.GetLength(1), plateau.Cases.GetLength(0)) {}
 
         public Case this[int x, int y] => Cases[x, y];
 
